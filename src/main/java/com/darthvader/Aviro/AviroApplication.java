fbc9c9ac4d5b839c885637e4c52ce60328cd4fab
Prashant Kumar Moroliya <25551044+prashantmoroliya@users.noreply.github.com>
2023-06-04 03:03:27 +0530

    initial commit

---
+++ src/main/java/com/darthvader/Aviro/AviroApplication.java
@@ -0,0 +1,13 @@
+package com.darthvader.Aviro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviroApplication.class, args);
+	}
+
+}
